cfed2921a5988a1e58c03ea310519cd293b009c1
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-09-20 10:12:27 +0530

    Pattern

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Pattern6 {
+    // 5
+    // *       *       *               *       *       *
+    // *       *                               *       *
+    // *                                               *
+    // *       *                               *       *
+    // *       *       *               *       *       *
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n=scn.nextInt();
+        int st=(n/2)+1;
+        int sp=1;
+        for(int i=0;i<n;i++){
+           for(int j=0;j<st;j++){
+                System.out.print("*\t");
+           }
+           
+           for(int j=0;j<sp;j++){
+                System.out.print("\t");
+           }
+           
+           for(int j=0;j<st;j++){
+                System.out.print("*\t");
+           }
+            if(i<n/2){
+            st--;
+            sp=sp+2;
+            }else{
+                st++;
+                sp=sp-2;
+            }
+             System.out.println();
+        }
+
+    }
+}
