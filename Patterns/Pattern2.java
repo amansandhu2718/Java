cc4ab119908c4b413e924fd73bcd08986a22f59d
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-09-20 09:43:30 +0530

    Pattern

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pattern2 {
+    // *       *       *       *       *
+    // *       *       *       *
+    // *       *       *
+    // *       *
+    // *
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+       int n=scn.nextInt();
+for(int i=1;i<=n;i++){
+    for(int j=i;j<=n;j++){
+        System.out.print("*\t");
+    }System.out.println();
+}
+    }
+}
