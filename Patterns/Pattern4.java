4268a129654eae6102d75853edc36c1550c4fb6a
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-09-20 09:55:29 +0530

    Pattern

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Pattern4 {
+    // *       *       *       *       *
+    //         *       *       *       *
+    //                 *       *       *
+    //                         *       *
+    //                                 *
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n=scn.nextInt();
+        int st=n;
+        int sp=0;
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<sp;j++){
+                System.out.print("\t");
+            }
+            
+            for(int j=0;j<st;j++){
+                 System.out.print("*\t");
+            }
+             System.out.println();
+             sp++;
+             st--;
+        }
+
+    }
+}
---
+++ Patterns/Pattern5.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Pattern5 {
+//                   *
+//           *       *       *
+//   *       *       *       *       *
+//           *       *       *
+//                   *
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n=scn.nextInt();
+        int sp=n/2;
+        int st=1;
+        for(int i=0;i<n;i++){
+            
+          for(int j=0;j<sp;j++){
+                System.out.print("\t");
+          }
+          
+          for(int j=0;j<st;j++){
+                System.out.print("*\t");
+          }
+          
+          for(int j=0;j<sp;j++){
+                System.out.print("\t");
+          }
+            
+            if(i<n/2){
+                sp--;
+                st=st+2;
+                
+            }else{
+                st-=2;
+                sp++;
+                
+            }
+            System.out.println();
+        }
+    }
+}
