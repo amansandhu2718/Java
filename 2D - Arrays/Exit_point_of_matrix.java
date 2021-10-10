b1e37909ed2104fe1b61c21d279a88ded2d07fde
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-10-10 19:34:35 +0530

    2D Array

---
+++ 2D - Arrays/Exit_point_of_matrix.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Exit_point_of_matrix {
+
+    public static void main(String[] args) throws Exception {
+        // The player moves in the same direction 
+        //as long as he meets '0'. On seeing a 1,
+        //he takes a 90 deg right turn.
+ 
+        Scanner scn=new Scanner(System.in);
+   int n=scn.nextInt();
+   int m=scn.nextInt();
+   int arr[][]=new int[n][m];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+           arr[i][j]=scn.nextInt();
+       }
+   }
+   int i=0;
+   int j=0;
+   int dir=0;
+   while(true){
+       dir=(dir+arr[i][j])%4;
+       if(dir==0){
+           j++;
+           if(j==arr[0].length){
+               j--;
+               break;
+           }
+       }else if(dir==1){
+           i++;
+           
+           if(i==arr.length){
+               i--;
+               break;
+           }
+       }else if(dir==2){
+           j--;
+           
+           if(j==-1){
+               j++;
+               break;
+           }
+       }else{
+           i--;
+           
+           if(i==-1){
+               i--;
+               break;
+           }
+       }
+   }
+   System.out.println(i);
+   System.out.println(j);
+    }
+
+}
