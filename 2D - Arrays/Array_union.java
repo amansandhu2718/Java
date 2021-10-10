87b1b5a481b17eed02e1e69f90d5c932c7945403
amansandhu2718 <77589604+amansandhu2718@users.noreply.github.com>
2021-10-10 20:11:44 +0530

    Array

---
+++ 2D - Arrays/Array_union.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Array_union {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int arr1[] = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = scn.nextInt();
+        }
+        int n2 = scn.nextInt();
+        int arr2[] = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            arr2[2] = scn.nextInt();
+        }
+        
+    }
+}
