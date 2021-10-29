76227d889ccbb6eb7c6e3864281dede0be13e601
Amandeep Singh <a9560301656@gmail.com>
2021-10-29 12:11:51 -0700

    basic

---
+++ BASIC (basic programs + functions)/Print_Z.java
@@ -0,0 +1,11 @@
+
+
+public class Print_Z {
+
+    public static void main(String[] args) {
+System.out.println("*****");
+System.out.println("   * ");
+System.out.println("  *  ");
+System.out.println(" *   ");
+System.out.println("*****");    }
+}
